e9694cb9cf7bfbbbae7ee7dfd5e1ace617e25a67
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-02-06 12:36:42 +0100

    Made a HexClass

--- HexColorValue.cs
+++ HexColorValue.cs
@@ -4,7 +4,7 @@ namespace GeometricForms
 {
     public class HexColorValue
     {
-        private const string WrongHexSize = "El string con el hex debe tener una longitud de 6";
+        private const string WrongHex = "El string proporcionado no se identifica como un hex de color RGB";
         private const int UTFAword = 41;
         private const int UTFFword = 46;
         private const int UTFZeroWord = 30;
@@ -12,9 +12,9 @@ namespace GeometricForms
         public const int HexStringLimitSize = 6;
         private string hexString;
 
-        public HexColorValue()
+        public HexColorValue(string hexString)
         {
-
+            HexString = hexString;
         }
 
         public string HexString
@@ -22,13 +22,28 @@ namespace GeometricForms
             get { return this.hexString;}
             set
             {
-                if (value.Length != HexStringLimitSize)
-                {
-                    throw new Exception(WrongHexSize);
+                if (!ValidateHex(value)){
+                    throw new Exception(WrongHex);
                 }
-                for(int i = 0; i < UTFAWord; i++) { }
-                this.hexString = value;
+                hexString = value;
             }
+        }
+        public static bool ValidateHex(string hex)
+        {
+            hex = hex.ToUpper();
+            if (hex.Length != HexStringLimitSize) return false;
+
+            for(int i = 0; i< hex.Length; i++)
+            {
+                if (IsCharAHexValue(hex[i])) return true;
+            }
+            return false;
+            
+        }
+        public static bool IsCharAHexValue(char hexChar)
+        {
+            int utfCharCode = Convert.ToInt32(hexChar);
+            return utfCharCode >= UTFZeroWord || utfCharCode <= UTFNineWord || utfCharCode >= UTFAword || utfCharCode <= UTFFword;
         }
     }
 }
